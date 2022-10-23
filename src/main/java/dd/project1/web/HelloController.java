7493e505d6128180334c40e0a3beaa62a856fd84
uch0731 <uch0731@naver.com>
2022-10-23 23:22:47 +0900

    프로젝트1

---
+++ src/main/java/dd/project1/web/HelloController.java
@@ -0,0 +1,25 @@
+package dd.project1.web;
+
+import dd.project1.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+
+    public String hello () {
+
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+
+    public HelloResponseDto helloDto (@RequestParam("name") String name, @RequestParam("amount") int amount) {
+
+        return new HelloResponseDto(name, amount);
+    }
+}
+
